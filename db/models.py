ce2e418d6c22d0814199e9e92f6d94f60bb71401
dimanikodyuk <dimanikodyuk@gmail.com>
2023-02-10 10:58:18 +0200

    test

--- db/models.py
+++ db/models.py
@@ -20,16 +20,15 @@ def create_loan_checker(p_type_id):
 # Генерація тексту помилки і відправка
 def check_error(result_data):
     if result_data[0] == 1:
-        error_text = result_data[1]
-        error_type = result_data[2]
+        error_type = result_data[1]
+        error_text = result_data[2]
         logger_deltam_checker.info(f"Виявлено помилку: {error_text}")
 
         message = f"""❗❗❗<b>Виявлено помилку</b>❗❗❗
--------------------------------------------------------------------------
-↪  <b>Сервіс:</b> <i>{error_type}</i>
+
+🟦  <b>Сервіс:</b> <i>{error_type}</i>
 
 🟥 <b>Текст помилки:</b> <i>{error_text}</i> 
--------------------------------------------------------------------------
 """
         bot.send_message(nykodiuk_id, message, parse_mode="HTML")
 
