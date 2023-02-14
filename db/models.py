18f7986091e6bf1afc690d5af2206a8f264acded
dimanikodyuk <dimanikodyuk@gmail.com>
2023-02-14 16:47:36 +0200

    update data

--- db/models.py
+++ db/models.py
@@ -1,26 +1,30 @@
 import pymysql
-from config import host_delta, user_delta, password_delta, database_delta, telegram_bot, nykodiuk_id, pavluchenko_id, goncharuk_id, rovnyi_id
-from logs.logger import logger_deltam_checker
-conn = pymysql.connect(host=host_delta, port=3306, user=user_delta, passwd=password_delta, db=database_delta,
-                       autocommit=True, charset="utf8")
+import pymssql
 import telebot
+from config import host_delta, user_delta, password_delta, database_delta, telegram_bot, nykodiuk_id, pavluchenko_id\
+    , goncharuk_id, rovnyi_id, host_dlm, user_dlm, passowrd_dlm, database_dlm
+from logs.logger import logger_deltam_checker
+conn = pymysql.connect(host=host_delta, port=3306, user=user_delta, passwd=password_delta, db=database_delta, charset="utf8")
+conn_mssql = pymssql.connect(server=host_dlm, user=user_dlm, password=passowrd_dlm, database=database_dlm, charset='cp1251')
 
 bot = telebot.TeleBot(telegram_bot)
 
 
-# Процедура перевірки
-def create_loan_checker(p_type_id):
-    check = conn.cursor()
-    check_sql = f"CALL leads_api.alert_deltam_checker({p_type_id});"
-    check.execute(check_sql)
-    res = check.fetchall()
-    check.close()
-    return res[0]
+def get_active_config():
+    id_conf = []
+    conf = conn.cursor()
+    conf_sql = f"select id, db_id from leads_api.alert_deltam_config where is_active = 1"
+    conf.execute(conf_sql)
+    res = conf.fetchall()
+    conf.close()
+    for i in res:
+        id_conf.append(i)
+    return id_conf
 
 
 def get_checker_time(p_type_id):
     checker = conn.cursor()
-    checker_sql = f"""SELECT is_active, CAST(start_check_dt AS VARCHAR(100)) as dt_start, CAST(end_check_dt AS varchar(100)) AS dt_end
+    checker_sql = f"""SELECT CAST(start_check_dt AS VARCHAR(100)) as dt_start, CAST(end_check_dt AS varchar(100)) AS dt_end
                         FROM leads_api.alert_deltam_config
                         WHERE id = {p_type_id};"""
     checker.execute(checker_sql)
@@ -29,20 +33,27 @@ def get_checker_time(p_type_id):
     return res
 
 
-def get_active_config():
-    id_conf = []
-    conf = conn.cursor()
-    conf_sql = f"select id from leads_api.alert_deltam_config where is_active = 1"
-    conf.execute(conf_sql)
-    res = conf.fetchall()
-    conf.close()
-    for i in res:
-        id_conf.append(i[0])
-    return id_conf
+# Процедура перевірки наявності помилок в БД leads_api (91 server)
+def create_loan_checker_leads_api(p_type_id):
+    check = conn.cursor()
+    check_sql = f"CALL leads_api.alert_deltam_checker({p_type_id});"
+    check.execute(check_sql)
+    res = check.fetchall()
+    check.close()
+    return res[0]
 
 
-# Генерація тексту помилки і відправка
-def check_error(result_data):
+# Процедура перевірки наявності помилок в БД crm (92 server)
+def create_loan_checker_crm(p_type_id):
+    checker = conn_mssql.cursor()
+    checker_sql = f"EXEC crm..[alert_deltam_checker] {p_type_id}"
+    checker.execute(checker_sql)
+    res = checker.fetchall()
+    checker.close()
+    return res
+
+# Генерація тексту помилки і відправка з 91 серверу
+def check_error_leads_api(result_data):
     if result_data[0] == 1:
         error_type = result_data[1]
         error_text = result_data[2]
@@ -59,3 +70,27 @@ def check_error(result_data):
         bot.send_message(goncharuk_id, message, parse_mode="HTML")
         bot.send_message(rovnyi_id, message, parse_mode="HTML")
 
+
+# Генерація тексту помилки і відправка з 92 серверу
+def check_error_crm(result_data):
+    if result_data[0] == 1:
+        error_type = result_data[1]
+        error_text = result_data[2]
+        error_lead = result_data[3]
+        error_contract_num = result_data[4]
+        logger_deltam_checker.info(f"Виявлено помилку: {error_text}")
+
+        message = f"""❗❗❗<b>Виявлено помилку</b>❗❗❗
+
+🟦  <b>Сервіс:</b> <i>{error_type}</i>
+
+✏  <b>LEAD_ID:</b> <i>{error_lead}</i>
+
+📄  <b>Договір:</b> <i>{error_contract_num}</i>
+
+🟥 <b>Текст помилки:</b> <i>{error_text}</i> 
+    """
+        bot.send_message(nykodiuk_id, message, parse_mode="HTML")
+        bot.send_message(pavluchenko_id, message, parse_mode="HTML")
+        bot.send_message(goncharuk_id, message, parse_mode="HTML")
+        bot.send_message(rovnyi_id, message, parse_mode="HTML")
--- main.py
+++ main.py
@@ -1,25 +1,35 @@
 from logs.logger import logger_deltam_checker
-from db.models import create_loan_checker, check_error, get_checker_time, get_active_config
+from db.models import get_active_config, get_checker_time, create_loan_checker_leads_api, check_error_leads_api, \
+    create_loan_checker_crm, check_error_crm
 from datetime import datetime
 
 
 if __name__ == "__main__":
     try:
-
+        # Витягуємо всі активні конфігурації з БД
         active_conf = get_active_config()
+        print(f"Час запуску: {datetime.now()}")
+        print("--------------------------------------------------------")
         for j in active_conf:
-            print(f"Запуск перевірки з конфігу № {j}")
+            print(f"Запуск перевірки з конфігу № {j[0]}")
             # Поточний час
             current_time = datetime.now().time()
-
-            # Статус, час запуску, час закінчення перевірок з конфігу за ід
-            res = get_checker_time(j)
-            time_start = datetime.strptime(res[1], '%H:%M:%S')
-            time_end = datetime.strptime(res[2], '%H:%M:%S')
-
-            # Якщо поточний час > за час початку перевірки а також поточний час < за час закінчення перевірки
-            if res[0] == 1 and current_time >= time_start.time() and current_time <= time_end.time():
-                check_error(create_loan_checker(j))
+            res = get_checker_time(j[0])
+            # Час запуску перевірки
+            time_start = datetime.strptime(res[0], '%H:%M:%S')
+            # Час зупинки перевірки
+            time_end = datetime.strptime(res[1], '%H:%M:%S')
+            # Якщо статус конфігу = 1, а також поточний час > за час початку перевірки а також поточний час < за час закінчення перевірки
+            if current_time >= time_start.time() and current_time <= time_end.time():
+                # Якщо БД leads_api
+                if j[1] == 1:
+                    check_error_leads_api(create_loan_checker_leads_api(j[0]))
+                # Якщо БД crm
+                elif j[1] == 2:
+                    result = create_loan_checker_crm(j[0])
+                    for i in result:
+                        check_error_crm(i)
+        print("--------------------------------------------------------")
 
     except ValueError as err:
         logger_deltam_checker.error("Помилка даних main.py: " + str(err))
